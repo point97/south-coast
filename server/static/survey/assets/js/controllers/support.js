1b44eb12a6463169240484b9a0ac5f80a39aa06b
Scott Fletcher <sfletche@gmail.com>
2014-03-21 16:01:05 -0700

    ensuring username is visible in left nav when referenced from support page

--- server/static/survey/assets/js/controllers/support.js
+++ server/static/survey/assets/js/controllers/support.js
@@ -3,6 +3,12 @@ angular.module('askApp')
   .controller('SupportCtrl', ['$scope', '$location', '$http', 'storage', function MainCtrl($scope, $location, $http, storage) {
     $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
 
+    if (app.user) {
+        $scope.user = app.user;
+    } else {
+        $scope.user = false;
+    }
+    
     $scope.title = "Support";
 
     $scope.version = app.version;
