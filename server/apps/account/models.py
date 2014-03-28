d01694b3e3f1959ed82d58402e5b27af494f0a5d
Scott Fletcher <sfletche@gmail.com>
2014-03-28 14:36:47 -0700

    reverting api key generation to previous strategy (which failed on initial deployment, but may be necessary now…)

--- server/apps/account/models.py
+++ server/apps/account/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 import datetime
 from django.dispatch import receiver
-from django.db.models.signals import post_save
+from tastypie.models import create_api_key
 
 
 class UserProfile(models.Model):
@@ -13,15 +13,7 @@ class UserProfile(models.Model):
         return "%s" % (self.user.username)
 
 User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
-#models.signals.post_save.connect(create_api_key, sender=User)
-
-@receiver(post_save, sender=User)
-def create_user_api_key(sender, **kwargs):
-    """
-    Auto-create ApiKey objects using Tastypie's create_api_key    
-    """
-    from tastypie.models import create_api_key
-    create_api_key(User, **kwargs)
+models.signals.post_save.connect(create_api_key, sender=User)
 
 
 class Feedback(models.Model):
