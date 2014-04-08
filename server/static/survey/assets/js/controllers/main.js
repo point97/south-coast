0479a61e52f8f49a6e8e1c63550f68bbd5612b39
Scott Fletcher <sfletche@gmail.com>
2014-04-08 16:53:45 -0700

    updating version comparisons so 1.0.10 > 1.0.9

--- server/static/survey/assets/js/controllers/main.js
+++ server/static/survey/assets/js/controllers/main.js
@@ -3,6 +3,52 @@ angular.module('askApp')
   .controller('MainCtrl', ['$scope', '$location', '$http', 'storage', function MainCtrl($scope, $location, $http, storage) {
     $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
 
+    function versionCompare(v1, v2, options) {
+        var lexicographical = options && options.lexicographical,
+            zeroExtend = options && options.zeroExtend,
+            v1parts = v1.split('.'),
+            v2parts = v2.split('.');
+
+        function isValidPart(x) {
+            return (lexicographical ? /^\d+[A-Za-z]*$/ : /^\d+$/).test(x);
+        }
+
+        if (!v1parts.every(isValidPart) || !v2parts.every(isValidPart)) {
+            return NaN;
+        }
+
+        if (zeroExtend) {
+            while (v1parts.length < v2parts.length) v1parts.push("0");
+            while (v2parts.length < v1parts.length) v2parts.push("0");
+        }
+
+        if (!lexicographical) {
+            v1parts = v1parts.map(Number);
+            v2parts = v2parts.map(Number);
+        }
+
+        for (var i = 0; i < v1parts.length; ++i) {
+            if (v2parts.length == i) {
+                return 1;
+            }
+
+            if (v1parts[i] == v2parts[i]) {
+                continue;
+            } else if (v1parts[i] > v2parts[i]) {
+                return 1;
+            } else {
+                return -1;
+            }
+        }
+
+        if (v1parts.length != v2parts.length) {
+            return -1;
+        }
+
+        return 0;
+    }
+
+
     $scope.path = 'home';
     $scope.title = "Trips";
     
@@ -39,7 +85,8 @@ angular.module('askApp')
     })
         .success(function (data) {
             $scope.newVersion = data.version;
-            if (app.version < data.version) {
+            // if (app.version < data.version) {
+            if (versionCompare($scope.version, $scope.newVersion) < 0) {
                 $scope.update = "An update is available for Digital Deck."
             } else {
                 $scope.update = false;
